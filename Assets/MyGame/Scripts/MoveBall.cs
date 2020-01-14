935303378ae0a3d60c9592544dc0d114c8268d32
Noah Hatz <Noah@Noah-Hatz-MacBook.local>
2020-01-14 15:11:30 +0100

    Finish Script and attach it to Object

---
+++ Assets/MyGame/Scripts/MoveBall.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class MoveBall : MonoBehaviour
+{
+    //public GameObject PosBallTopLeft;
+    //public GameObject PosBallTopRight;
+    //public GameObject PosBallBottomLeft;
+    //public GameObject PosBallBottomRight;
+
+    [SerializeField] private GameObject[] ballPositions;
+    [SerializeField] private GameObject Ball;
+
+   public void MoveTheBall()
+   {
+       Debug.Log(Random.Range(0,1));
+   }
+}
